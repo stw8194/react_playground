bb85bd255f23a3fa1c7a16171a4d52460230aebf
stw8194 <stw8194@gmail.com>
2025-01-03 21:20:40 +0900

    chore: change folder name from use to useState for another folder name

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { App } from "./use/useTabs";
-// import { App } from "./use/useInput";
-// import { App, AppUgly } from "./useState";
+import { App } from "./useState/useTabs";
+// import { App } from "./useState/useInput";
+// import { App, AppUgly } from "./useState/useState";
 
 const rootElement = document.getElementById("root")!;
 const root = ReactDOM.createRoot(rootElement);
